2313d2b439a02578b1733227dd9d74bc4706dd66
Alberto <albertoig8@hotmail.com>
2021-05-31 18:32:14 -0300

    Adicionando try em ini.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,3 +10,17 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    if test_config is None:
+        # carregue a configuração da instância, se existir, quando não estiver testando
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # carregue a configuração do teste se for aprovado
+        app.config.from_mapping(test_config)
+
+    # certifique-se de que a pasta da instância existe
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
